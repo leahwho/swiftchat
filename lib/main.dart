6d19ff7c75755376715866182b71c2cac9a5de9a
Leah Hughes <leahwhughes@gmail.com>
2020-07-07 15:40:12 -0700

    adds routes for nav, moves code to homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,20 @@
 import 'package:flutter/material.dart';
+import 'quick_board.dart';
+import 'home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SwiftChat());
 }
 
-class MyApp extends StatelessWidget {
+class SwiftChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          // title: Text('SwiftChat'),
-          backgroundColor: Colors.purple[900],
-        ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 100),
-              Image(
-                image: AssetImage(
-                  'images/DeepPurpleCircleLogo.png',
-                ),
-              ),
-              const SizedBox(height: 100), // invisible
-              RaisedButton(
-                onPressed: () {}, // TODO: Add function!
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 10, bottom: 10),
-                  child: Text('Quick Board', style: TextStyle(fontSize: 25)),
-                ),
-                color: Colors.purple[200],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              RaisedButton(
-                onPressed: () {}, // TODO: Add function!
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 10, bottom: 10),
-                  child: Text('Login', style: TextStyle(fontSize: 25)),
-                ),
-                color: Colors.purple[200],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              RaisedButton(
-                onPressed: () {}, // TODO: Add function!
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 10, bottom: 10),
-                  child: Text('Register', style: TextStyle(fontSize: 25)),
-                ),
-                color: Colors.purple[200],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12.0),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/quickboard': (context) => QuickBoard(),
+      },
     );
   }
 }
