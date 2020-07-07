803bc65768d9a1ef3aa15bbc5a3d9b17aaf7169d
Leah Hughes <leahwhughes@gmail.com>
2020-07-07 15:40:46 -0700

    adds page for nav test

---
+++ lib/quick_board.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuickBoard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      body: Text('This is going to be the quick board page'),
+    );
+  }
+}
+
+// TODO 1: get buttons on this page with some placeholder text
