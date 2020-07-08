269fa76b7bd43851f09fbecc3ffd8b6b72759314
Leah Hughes <leahwhughes@gmail.com>
2020-07-07 20:23:28 -0700

    adds bottom nav bar to quick board

--- lib/quick_board.dart
+++ lib/quick_board.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'bottom_nav_bar.dart';
 import 'board_button.dart';
 
 class QuickBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //TODO: replace with AppBar widget
       appBar: AppBar(
-        title: Text(
-          'QuickBoard',
-          style: TextStyle(color: Colors.black),
-        ),
-        backgroundColor: Colors.white,
+        title: Text('SwiftChat'),
+        backgroundColor: Colors.grey,
       ),
+      bottomNavigationBar: BottomNavBar(),
       body: Column(
         children: <Widget>[
           BoardButton(),
