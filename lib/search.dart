fc6a8d16b1ee92bd0abe65b9e35237f2fd14f21d
Leah Hughes <leahwhughes@gmail.com>
2020-07-10 09:35:33 -0700

    search functionality is actually working?

--- lib/search.dart
+++ lib/search.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flappy_search_bar/flappy_search_bar.dart';
+import 'package:http/http.dart' as http; // this names the import http
+
+import 'dart:convert';
+
 import 'app_bar.dart';
 import 'bottom_nav_bar.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http; // this names the import http
+import 'src.dart';
 
 class SwiftSearch extends StatefulWidget {
   @override
@@ -11,15 +13,17 @@ class SwiftSearch extends StatefulWidget {
 }
 
 class _SwiftSearchState extends State<SwiftSearch> {
+  String userQuery;
+  Map data;
   String imageAddress;
   bool displayImages = false;
 
-  Future<void> getResults(query) async {
+  Future<void> getResults(userQuery) async {
     String imgThumbnail;
 
     try {
       http.Response response = await http.get(
-          'https://www.googleapis.com/customsearch/v1?key=AIzaSyA0KRsDb701uyxrEM_y1HqblO1dyatyh-U&cx=010969644493142160731:fdiwgadkj5k&q=$query');
+          'https://www.googleapis.com/customsearch/v1?key=$apiKey&cx=$cseId=$userQuery');
 
       if (response.statusCode == 200) {
         var data = response.body;
@@ -37,6 +41,17 @@ class _SwiftSearchState extends State<SwiftSearch> {
     }
 
     imageAddress = imgThumbnail;
+  }
+
+  Future<void> getSearchResults(userQuery) async {
+    String url =
+        'https://www.googleapis.com/customsearch/v1?key=$apiKey&cx=$cseId=$userQuery';
+
+    http.Response response = await http.get(url);
+
+    this.setState(() {
+      data = json.decode(response.body);
+    });
   }
 
   @override
@@ -57,11 +72,43 @@ class _SwiftSearchState extends State<SwiftSearch> {
                     ),
                     hintText: 'Enter Search Query',
                   ),
-                  onSubmitted: (value) {
-                    getResults(value);
+                  onSubmitted: (query) {
+                    getSearchResults(query);
+                    //getResults(query);
+                  },
+                  onChanged: (query) {
+                    setState(() {
+                      displayImages = false;
+                      userQuery = query;
+                    });
                   },
                 ),
               ),
+              Container(
+                child: GridView.builder(
+                  itemCount: data == null ? 0 : data.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Card(
+                          child: Image.network(
+                              '${data['items'][index]['pagemap']['cse_thumbnail'][0]['src']}')),
+                    );
+                  },
+                  shrinkWrap: true,
+                ),
+              ),
+              // FutureBuilder(
+              //   future: getSearchResults(query),
+              //   builder: (context, snapShot) {
+              //   if (snapShot.hasError) {
+              //     return Text('Error! Ack! No images for you!');
+              //   } else if (snapShot.hasData) {
+              //     return Text('YOU HAVE DATA!');
+              //   }
+              // }),
               displayImages
                   ? Card(child: Image.network(imageAddress))
                   : Container()
@@ -73,5 +120,36 @@ class _SwiftSearchState extends State<SwiftSearch> {
   }
 }
 
+// return GridView.builder(
+//                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                           crossAxisCount: 3),
 
+//                       itemBuilder: (BuildContext context, int index) {
+//                         return Container();
+//                       });
 
+// FutureBuilder(
+//                 future: getSearchResults(),
+//                 builder: (context, snapShot) {
+//                   Map data = snapShot.data;
+//                   if(snapShot.hasError) {
+//                     print(snapShot.error);
+//                     return Text('Failed to get response with this error: ${snapShot.error}')
+//                   } else if (snapShot.hasData) {
+//                     return new Center(
+//                       child: GridView.count(
+//                         crossAxisCount: 3,
+//                         children: List.generate(30, (index) {
+//                           return Center(
+//                             child: Image.network('${data['items'][index]['pagemap']['cse_thumbnail']
+//                               [0]['src']}'),
+//                             );
+//                           }
+//                         ),
+//                         )
+//                     );
+//                   } else if (!snapShot.hasData){
+//                     return Center(child: CircularProgressIndicator());
+//                   }
+//                 }
+//                 ),
