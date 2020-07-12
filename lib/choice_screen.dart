8ac1ac59dd4a66f3d48678f9aacb5d04fb539e65
Leah Hughes <leahwhughes@gmail.com>
2020-07-11 18:10:03 -0700

    a brand new page for displaying choice!

---
+++ lib/choice_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'app_bar.dart';
+import 'bottom_nav_bar.dart';
+
+class ChoiceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF293241),
+      appBar: SwiftAppBar('Choice!'),
+      bottomNavigationBar: BottomNavBar(),
+      body: Text('Your choice is gonna be right here'),
+    );
+  }
+}
