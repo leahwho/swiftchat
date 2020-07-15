5c4810a61fbfba3e8d92a5fce9295a94a1db4587
Leah Hughes <leahwhughes@gmail.com>
2020-07-15 09:05:02 -0700

    using horizontal button layout - not great

--- lib/four_board.dart
+++ lib/four_board.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:swift_chat/app_bar.dart';
-import 'bottom_nav_bar.dart';
-import 'board_button.dart';
+import 'package:swift_chat/horiz_button.dart';
 import 'boom_menu.dart';
+import 'horiz_button.dart';
 
 class FourBoard extends StatelessWidget {
   static String id = 'four_board';
@@ -20,10 +20,10 @@ class FourBoard extends StatelessWidget {
         ),
         child: Column(
           children: <Widget>[
-            BoardButton(),
-            BoardButton(),
-            BoardButton(),
-            BoardButton(),
+            HorizButton(),
+            HorizButton(),
+            HorizButton(),
+            HorizButton(),
           ],
         ),
       ),
--- lib/three_board.dart
+++ lib/three_board.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:swift_chat/app_bar.dart';
 import 'package:swift_chat/boom_menu.dart';
+import 'package:swift_chat/horiz_button.dart';
 import 'bottom_nav_bar.dart';
 import 'board_button.dart';
 import 'boom_menu.dart';
+import 'horiz_button.dart';
 
 class ThreeBoard extends StatelessWidget {
   static String id = 'three_board';
@@ -18,9 +20,9 @@ class ThreeBoard extends StatelessWidget {
         padding: EdgeInsets.only(bottom: 75.0),
         child: Column(
           children: <Widget>[
-            BoardButton(),
-            BoardButton(),
-            BoardButton(),
+            HorizButton(),
+            HorizButton(),
+            HorizButton(),
           ],
         ),
       ),
