86041dfbe3a22539ffa768364840c54193a6af77
Leah Hughes <leahwhughes@gmail.com>
2020-07-16 15:57:01 -0700

    lifting state from button to board

--- lib/board_button.dart
+++ lib/board_button.dart
@@ -3,28 +3,21 @@ import 'choice_screen.dart';
 import 'search.dart';
 import 'data.dart';
 
+class BoardButton extends StatelessWidget {
+  BoardButton(
+      {this.id,
+      this.imgUrl,
+      this.userQuery,
+      this.displayToggle,
+      this.searchResults,
+      this.onClearClick});
 
-class BoardButton extends StatefulWidget {
-  @override
-  _BoardButtonState createState() => _BoardButtonState();
-}
-
-class _BoardButtonState extends State<BoardButton> {
-  String imgUrl;
-  String userQuery;
-  bool displayToggle = false;
-
-  void updateCard(dynamic searchResults) {
-    if (searchResults == null) {
-      print('your search results are null! ugh!');
-    } else {
-      setState(() {
-        displayToggle = true;
-        imgUrl = searchResults['imgUrl'];
-        userQuery = searchResults['userQuery'];
-      });
-    }
-  }
+  final int id;
+  final String imgUrl;
+  final String userQuery;
+  final bool displayToggle;
+  final Function searchResults;
+  final Function onClearClick;
 
   @override
   Widget build(BuildContext context) {
@@ -78,9 +71,7 @@ class _BoardButtonState extends State<BoardButton> {
                   icon: Icon(Icons.clear),
                   onPressed: () {
                     print('Clear was pressed!');
-                    setState(() {
-                      displayToggle = false;
-                    });
+                    this.onClearClick();
                   },
                 ),
               ),
@@ -92,7 +83,7 @@ class _BoardButtonState extends State<BoardButton> {
                     print('search was pressed');
                     var searchResults =
                         await Navigator.pushNamed(context, SwiftSearch.id);
-                    updateCard(searchResults);
+                    this.searchResults(searchResults, id);
                   },
                 ),
               ),
@@ -103,92 +94,3 @@ class _BoardButtonState extends State<BoardButton> {
     );
   }
 }
-
-
-// Stateless option:
-// class BoardButton extends StatelessWidget {
-//   BoardButton(
-//       {this.imgUrl, this.userQuery, this.updateCard, this.displayToggle});
-
-//   final String imgUrl;
-//   final String userQuery;
-//   final void updateCard;
-//   final bool displayToggle;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Expanded(
-//       child: Padding(
-//         padding: EdgeInsets.all(8.0),
-//         child: Card(
-//           color: Color(0xFFCfDBD5),
-//           margin: EdgeInsets.all(2.0),
-//           child: Stack(
-//             children: <Widget>[
-//               Align(
-//                 alignment: Alignment.center,
-//                 child: Padding(
-//                   padding: const EdgeInsets.all(8.0),
-//                   child: ClipRRect(
-//                     borderRadius: BorderRadius.all(Radius.circular(5.0)),
-//                     child: GestureDetector(
-//                       onTap: () {
-//                         print('Image was clicked!');
-
-//                         Data data =
-//                             new Data(imgUrl: imgUrl, userQuery: userQuery);
-//                         Navigator.pushNamed(context, ChoiceScreen.id,
-//                             arguments: data);
-//                       },
-//                       child: Container(
-//                         height: 225,
-//                         padding: EdgeInsets.only(
-//                           bottom: 10.0,
-//                         ),
-//                         child:
-//                             displayToggle ? Image.network(imgUrl) : Container(),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               Align(
-//                 alignment: Alignment.bottomCenter,
-//                 child: Padding(
-//                   padding: EdgeInsets.only(bottom: 10.0),
-//                   child: displayToggle
-//                       ? Text(userQuery, style: TextStyle(fontSize: 25.0))
-//                       : Text(''),
-//                 ),
-//               ),
-//               Align(
-//                 alignment: Alignment.topRight,
-//                 child: IconButton(
-//                   icon: Icon(Icons.clear),
-//                   onPressed: () {
-//                     print('Clear was pressed!');
-//                     setState(() {
-//                       displayToggle = false;
-//                     });
-//                   },
-//                 ),
-//               ),
-//               Align(
-//                 alignment: Alignment.bottomRight,
-//                 child: IconButton(
-//                   icon: Icon(Icons.search),
-//                   onPressed: () async {
-//                     print('search was pressed');
-//                     var searchResults =
-//                         await Navigator.pushNamed(context, SwiftSearch.id);
-//                     updateCard(searchResults);
-//                   },
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
--- lib/two_board.dart
+++ lib/two_board.dart
@@ -15,8 +15,80 @@ class TwoBoard extends StatefulWidget {
 class _TwoBoardState extends State<TwoBoard> {
   final firestoreInstance = Firestore.instance;
 
+  List buttonCollection = [];
+  String imgUrl;
+  String userQuery;
+  bool displayToggle = false;
+
+  @override
+  void initState() {
+    super.initState();
+    generateButtons();
+  }
+
+  List generateButtons() {
+    List buttons = [];
+    int currentId = 0;
+
+    for (int i = 0; i < 2; i++) {
+      print(currentId);
+      buttons.add(
+        {
+          'imgUrl': '',
+          'userQuery': '',
+          'id': currentId,
+          'displayToggle': false,
+        },
+      );
+      currentId++;
+      print(currentId);
+    }
+    print('here is buttons from generateButtons: $buttons');
+
+    setState(() {
+      buttonCollection = buttons;
+    });
+  }
+
+  void searchResultsCallback(dynamic searchResults, id) {
+    print('this is search results from callback $searchResults');
+    print(
+        'this is button collection from callback before setstate: $buttonCollection');
+    print('this is id from callback: $id');
+
+    List newButtons = [];
+
+    if (searchResults == null) {
+      print('your search results are null! ugh! better luck next time');
+    } else {
+      for (var button in buttonCollection) {
+        if (button['id'] == id) {
+          button['imgUrl'] = searchResults['imgUrl'];
+          button['userQuery'] = searchResults['userQuery'];
+          button['displayToggle'] = true;
+          newButtons.add(button);
+        } else {
+          newButtons.add(button);
+        }
+      }
+    }
+    setState(() {
+      buttonCollection = newButtons;
+    });
+    print(
+        'this is button collection from callback after setstate: $buttonCollection');
+  }
+
+  void clearClick() {
+    setState(() {
+      displayToggle = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(
+        ' this is button collection from inside BuildContext: $buttonCollection');
     return Scaffold(
       backgroundColor: Color(0xFF293241),
       appBar: SwiftAppBar('Two Choices'),
@@ -28,12 +100,18 @@ class _TwoBoardState extends State<TwoBoard> {
         ),
         child: Column(
           children: <Widget>[
-            BoardButton(),
-            BoardButton(),
+            for (var button in buttonCollection)
+              BoardButton(
+                imgUrl: button['imgUrl'],
+                userQuery: button['userQuery'],
+                id: button['id'],
+                displayToggle: button['displayToggle'],
+                searchResults: searchResultsCallback,
+                onClearClick: clearClick,
+              )
           ],
         ),
       ),
     );
   }
 }
-
