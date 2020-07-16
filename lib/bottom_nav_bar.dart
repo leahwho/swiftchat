998cbc5ddc98af650596d2a362026184ed52306d
Leah Hughes <leahwhughes@gmail.com>
2020-07-16 09:30:32 -0700

    initial firestore database call with hardcode data

--- lib/bottom_nav_bar.dart
+++ lib/bottom_nav_bar.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:swift_chat/data.dart';
+import 'home_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BottomNavBar extends StatelessWidget {
+  final firestoreInstance = Firestore.instance;
+
+// this will work; now how do you get the data you need to save from the board to the nav bar..?
+  void saveBoard() {
+    firestoreInstance.collection("boards").add({
+      "board_id": 'a test board',
+      "board": {
+        'card1': {
+          'imgUrl': 'www.sometestimage.com',
+          'userQuery': 'the user query'
+        },
+        'card2': {
+          'imgUrl': 'www.sometestimage.com',
+          'userQuery': 'the user query'
+        },
+      },
+    }).then((value) {
+      print(value.documentID);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
@@ -11,14 +35,18 @@ class BottomNavBar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           IconButton(
-            icon: Icon(Icons.menu),
+            icon: Icon(Icons.home),
             color: Colors.white,
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pushNamed(context, HomeScreen.id);
+            },
           ),
           IconButton(
-            icon: Icon(Icons.clear),
+            icon: Icon(Icons.save),
             color: Colors.white,
-            onPressed: () {},
+            onPressed: () {
+              saveBoard();
+            },
           ),
         ],
       ),
