1aedaa9486c700a18e22864cab1a5a0632de7a94
Leah Hughes <leahwhughes@gmail.com>
2020-07-22 09:18:05 -0700

    logout button only visible when user logged in

--- lib/app_bar.dart
+++ lib/app_bar.dart
@@ -2,13 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'home_screen.dart';
 
-class SwiftAppBar extends StatelessWidget implements PreferredSizeWidget {
-  Size get preferredSize => new Size.fromHeight(kToolbarHeight);
+class SwiftAppBar extends StatefulWidget implements PreferredSizeWidget {
+  final String title;
 
   SwiftAppBar(this.title);
+  
+  Size get preferredSize => Size.fromHeight(50);
 
-  final String title;
-  final _auth = FirebaseAuth.instance;
+  @override
+  _SwiftAppBarState createState() => _SwiftAppBarState();
+}
+
+class _SwiftAppBarState extends State<SwiftAppBar> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser loggedInUser;
+  bool userLoggedIn = false;
+
+  @override
+  void initState() {
+    getCurrentUser();
+    super.initState();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      // then you can display saved boards?
+      if (user != null) {
+        loggedInUser = user;
+        setState(() {
+          userLoggedIn = true;
+        });
+        print('${loggedInUser.email} is logged in');
+        return;
+      }
+    } catch (error) {
+      print(error);
+    }
+    print('there is no user logged in');
+  }
 
   void logOut() {
     if (_auth.currentUser() != null) {
@@ -20,13 +52,13 @@ class SwiftAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(
-        title,
+        widget.title,
         style: TextStyle(
           color: Color(0xFFe8eddf),
         ),
       ),
       actions: <Widget>[
-        IconButton(
+        userLoggedIn ? IconButton(
           icon: Icon(
             Icons.exit_to_app,
             color: Color(0xFFe8eddf),
@@ -35,9 +67,49 @@ class SwiftAppBar extends StatelessWidget implements PreferredSizeWidget {
             logOut();
             Navigator.pushNamed(context, HomeScreen.id);
           },
-        ),
+        ) : SizedBox(width: 0),
       ],
       backgroundColor: Color(0xFF293241),
     );
   }
 }
+
+// class SwiftAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   Size get preferredSize => new Size.fromHeight(kToolbarHeight);
+
+//   SwiftAppBar(this.title);
+
+//   final String title;
+//   final _auth = FirebaseAuth.instance;
+
+//   void logOut() {
+//     if (_auth.currentUser() != null) {
+//       _auth.signOut();
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       title: Text(
+//         title,
+//         style: TextStyle(
+//           color: Color(0xFFe8eddf),
+//         ),
+//       ),
+//       actions: <Widget>[
+//         IconButton(
+//           icon: Icon(
+//             Icons.exit_to_app,
+//             color: Color(0xFFe8eddf),
+//           ),
+//           onPressed: () {
+//             logOut();
+//             Navigator.pushNamed(context, HomeScreen.id);
+//           },
+//         ),
+//       ],
+//       backgroundColor: Color(0xFF293241),
+//     );
+//   }
+// }
